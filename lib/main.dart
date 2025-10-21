31df06b752e98bfa9baa285ea559f04f3dbb87a1
Yunus <yunusnotfound@icloud.com>
2025-10-21 23:18:27 +0300

    add home page ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_auth/pages/home_page.dart';
 import 'package:flutter_firebase_auth/pages/login_page.dart';
 import 'firebase_options.dart';
 
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: LoginPage(),
+      home: HomePage(),
     );
   }
 }
